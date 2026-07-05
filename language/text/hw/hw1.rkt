78b277570574877b89a100dc42615c42ab16b20c
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 11:15:52 +0100

    Add usage comments to hw1.rkt
    Added comments explaining how to run the program and expected output.

--- language/text/hw/hw1.rkt
+++ language/text/hw/hw1.rkt
@@ -1,2 +1,9 @@
 #lang racket
 "Hello, world!"
+; --------------------------------------------------------
+; using Linux Mint OS
+; this program can be run from the command line by typing:
+; racket hw1.rkt
+; the output should appear as:
+; "Hello, world!"
+
